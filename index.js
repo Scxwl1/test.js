e09f9d638deccb57a31fd592ac9ec20325b5f4ba
Danya <vip.danya0212@mail.ru>
2023-11-22 14:48:53 +0300

    test

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+function getSum(a, b) {
+    return a + b;
+    }
 
+function getSmallestNumber(a, b) {
+    if (a < b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+
+function castBoolean(value) {
+    return Boolean(value);
+}
+
+function capitalize(string, index) {
+    if (index < 0 || index >= string.length) {
+        return string;
+    }
+}
